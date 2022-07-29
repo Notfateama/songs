3a26abc90924fcc54a1e30321108c1e5effbcb3a
Notfateama <110249944+Notfateama@users.noreply.github.com>
2022-07-29 23:43:37 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+      name: "Prateek Kuhad - Kasoor",
+      artist: "Fatima",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Rochak Kohli - Dil Na Jaaneya",
+      artist: "Fatima",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "James Arthur - Say You Won't",
+      artist: "Fatima",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Saware X Agar Tum Sath Ho",
+      artist: "Fatima",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Lewis Capaldi - Hold Me",
+      artist: "Fatima",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Jeremy Zucker - Comethru",
+      artist: "Fatima",
+      img: "music-6",
+      src: "music-6"
+    },
+  ];
